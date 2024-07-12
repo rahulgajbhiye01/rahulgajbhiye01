10591f5be364da08301bb4fc6e2b1ce0b0e7f69f
Rahul Gajbhiye <rahulgajbhiye201@gmail.com>
2024-07-12 17:46:36 +0530

    update sitemap.xml

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,15 +1,25 @@
 import { MetadataRoute } from "next";
+import { getBlogsData } from "@/lib/db/db-helper";
 
-export default function sitemap(): MetadataRoute.Sitemap {
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  const Blogs = await getBlogsData();
   return [
     {
       url: `${process.env.NEXT_PUBLIC_BASE_URL}/`,
+      priority: 1,
     },
     {
       url: `${process.env.NEXT_PUBLIC_BASE_URL}/projects`,
+      changeFrequency: "weekly",
+      priority: 0.8,
     },
     {
       url: `${process.env.NEXT_PUBLIC_BASE_URL}/blogs`,
+      changeFrequency: "daily",
+      priority: 0.8,
     },
+    ...Blogs.map((o) => ({
+      url: `${process.env.NEXT_PUBLIC_BASE_URL}/${o.indexedTitle}`,
+    })),
   ];
 }

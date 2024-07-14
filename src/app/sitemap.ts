f7aee0738e2c62a583bc705fea1dbc2e9f0a291c
Rahul Gajbhiye <rahulgajbhiye201@gmail.com>
2024-07-14 21:08:37 +0530

    sitemap fix

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -9,17 +9,17 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
       priority: 1,
     },
     {
-      url: `${process.env.NEXT_PUBLIC_BASE_URL}/projects`,
+      url: `${process.env.NEXT_PUBLIC_BASE_URL}/work`,
       changeFrequency: "weekly",
       priority: 0.8,
     },
     {
-      url: `${process.env.NEXT_PUBLIC_BASE_URL}/blogs`,
+      url: `${process.env.NEXT_PUBLIC_BASE_URL}/blog`,
       changeFrequency: "daily",
       priority: 0.8,
     },
     ...Blogs.map((o) => ({
-      url: `${process.env.NEXT_PUBLIC_BASE_URL}/${o.indexedTitle}`,
+      url: `${process.env.NEXT_PUBLIC_BASE_URL}/blog/${o.indexedTitle}`,
     })),
   ];
 }

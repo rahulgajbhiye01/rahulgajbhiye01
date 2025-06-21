db1739d25800ff0d0ef92bc14c0868dbd576d97e
Rahul Gajbhiye <rahulgajbhiye201@gmail.com>
2025-06-21 05:31:14 +0530

    major change

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
+/** @type {import('next').NextConfig} */
 const nextConfig: NextConfig = {
-  /* config options here */
+  // Configure `pageExtensions` to include markdown and MDX files
+  pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
+  // Optionally, add any other Next.js config below
 };
 
 export default nextConfig;
